32bfe0a9aed54393a914871bf40ab2ff8bb1b97b
HomeMedia <taknado13@yandex.ru>
2024-02-12 14:32:13 +0700

    начало

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int apple = 10000;
+        byte knife = 2;
+        short bags = 15;
+        long cream = 100L;
+        float many = 100.5F;
+        double tea = 555.555;
+        System.out.println("Значение переменной apple с типом int равно " + apple);
+        System.out.println();
+    }
+}
